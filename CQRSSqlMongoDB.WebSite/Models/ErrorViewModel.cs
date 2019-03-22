0df06ae227d152afff6481ae587eb9cf601edd14
Tiago Avila <tiago.avila09@gmail.com>
2019-03-22 09:59:10 -0300

    Initial structure

---
+++ CQRSSqlMongoDB.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CQRSSqlMongoDB.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRQSSqlMongoDB.Domain/Customer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CQRSSqlMongoDB.Domain
+{
+    public class Customer
+    {
+    }
+}
